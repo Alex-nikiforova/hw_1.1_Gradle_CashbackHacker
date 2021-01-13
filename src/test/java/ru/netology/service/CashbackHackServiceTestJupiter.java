fb9f131db3b7954f9ce587592edab822c907e8d7
Alexandra Nikiforova <sunny.alej@gmail.com>
2021-01-13 13:27:39 +0300

    used API junit jupiter

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTestJupiter.java
@@ -0,0 +1,51 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CashbackHackServiceTestJupiter {
+
+    CashbackHackService service = new CashbackHackService();
+
+    @Test
+
+    public void shouldReturn500IfAmount500() {
+
+        int actual = service.remain(500);
+        int expected = 500;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldReturn1000IfAmount0() {
+
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldReturn100IfAmount1900() {
+
+        int actual = service.remain(1900);
+        int expected = 100;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+
+    public void shouldReturn0IfAmount1000() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        assertEquals(expected, actual);
+    }
+
+}
